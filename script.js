963a7c301933b0d2faba1327d08fa5e7c6d86e78
diamehta23 <154620279+diamehta23@users.noreply.github.com>
2025-05-12 21:08:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-function indexOfIgnoreCase(s1, s2) {
-  // write your code here
-}
 
-// Please do not change the code below
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
-alert(indexOfIgnoreCase(s1, s2));
+function indexOfIgnoreCase(str, subStr) {
+  // Convert both strings to lowercase for case-insensitive comparison
+  const lowerStr = str.toLowerCase();
+  const lowerSubStr = subStr.toLowerCase();
+  
+  // Use indexOf to find the first occurrence
+  return lowerStr.indexOf(lowerSubStr);
+}
